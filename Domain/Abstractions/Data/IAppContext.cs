4a98fa7f71ab5281862722c045666776561b5a71
Никита Каргаполов <nikita.kargapolov000@mail.ru>
2021-06-30 18:53:47 +0300

    Domain Data

---
+++ Domain/Abstractions/Data/IAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions.Data
+{
+    public interface IAppContext : IUnitOfWorkCreator
+    {
+    }
+}
---
+++ Domain/Abstractions/Data/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Apply();
+        Task Cancel();
+    }
+}
